6f2013e01f0a38347fc9738df56837fcf2515e5d
Willian Mano <willianmanoaraujo@gmail.com>
2020-07-11 18:27:14 -0300

    Add pt_br lang file

---
+++ lang/pt_br/profilefield_masked.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component profilefield_masked
+ *
+ * @package    profilefield_masked
+ * @copyright  2020 onwards Willian Mano {@link http://conecti.me}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Campo com máscara';
+$string['mask'] = 'Máscara';
+$string['mask_help'] = 'Digita a mácara que você deseja, por exemplo: 00/00/0000';
+$string['privacy:metadata:profilefield_masked:userid'] = 'O ID do usuário que o dado foi guardado pelo plugin campo com máscara';
+$string['privacy:metadata:profilefield_masked:fieldid'] = 'O ID do campo de perfil do usuário';
+$string['privacy:metadata:profilefield_masked:data'] = 'Dados do usuário do plugin campo com máscara';
+$string['privacy:metadata:profilefield_masked:dataformat'] = 'O format do dado do plugin campo com máscara';
+$string['privacy:metadata:profilefield_masked:tableexplanation'] = 'Dados adicionais do perfil';
